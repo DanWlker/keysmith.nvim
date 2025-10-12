f4060feeef4e2928eb1c70129db7ce3018bcb099
DanWlker <danielhee2@gmail.com>
2025-10-12 11:17:09 +0800

    remove unused functions

--- lua/keysmith/tools.lua
+++ lua/keysmith/tools.lua
@@ -1,19 +1,5 @@
 local M = {}
 
--- Credit to mini.nvim
----@param msg string
-M.error = function(msg) error('(keysmith) ' .. msg, 0) end
-
--- Credit to mini.nvim
----@param msg string
-M.error_treesitter = function(msg)
-  local bufnr, ft = vim.api.nvim_get_current_buf(), vim.bo.filetype
-  local has_lang, lang = pcall(vim.treesitter.language.get_lang, ft)
-  lang = has_lang and lang or ft
-  msg = string.format('Can not get %s for buffer %d and language "%s".', msg, bufnr, lang)
-  M.error(msg)
-end
-
 ---@param using_parser string
 ---@return Keysmith.NodeItem[] | nil
 M.get_all_leaf_keysmith_nodes = function(using_parser)
